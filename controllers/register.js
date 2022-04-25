607c4130c66fb6f2bfc45761bb6c7b460ddd8946
Ajanth Uthayan <uthayakumaran.ajanth@gmail.com>
2022-04-25 10:11:10 -0400

    edit database config

--- controllers/register.js
+++ controllers/register.js
@@ -16,13 +16,13 @@ const handleRegister = (db, bcrypt) => (req, res) => {
       .into("login")
       .returning("email")
       .then((loginEmail) => {
-        trx("users")
+        return trx("users")
+          .returning("*")
           .insert({
             email: loginEmail[0].email,
             name: name,
             joined: new Date(),
           })
-          .returning("*")
           .then((user) => res.json(user[0]))
           .then(trx.commit)
           .catch(trx.rollback);
